9d8281b72782715c1bd1e8615c2183cd44631a0b
tkling <tyling@gmail.com>
2023-08-03 08:41:42 -0400

    initial commit

---
+++ app/main.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+def tick(args)
+  # args ||= GTK::Args.new
+  # args.outputs.labels << [640, 540, 'Hello World!', 5, 1]
+  # args.outputs.labels << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
+  # args.outputs.labels << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+  #
+  # args.outputs.sprites << { x: 576,
+  #                           y: 280,
+  #                           w: 128,
+  #                           h: 101,
+  #                           path: 'dragonruby.png',
+  #                           angle: args.state.tick_count }
+  #
+  # args.outputs.labels << { x: 640,
+  #                          y: 60,
+  #                          text: './mygame/app/main.rb',
+  #                          size_enum: 5,
+  #                          alignment_enum: 1 }
+
+  handle_input(args)
+
+end
+
+def handle_input(args)
+  args.gtk.request_quit if args.inputs.keyboard.key_down.escape
+end
