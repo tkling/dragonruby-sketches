4ea9564d92148fb7c990a5563b3d26b14f44ce62
tkling <tyling@gmail.com>
2023-08-15 17:03:17 -0400

    start rebuilding spacebeing - assets, sounds, title screen

--- app/screens/level.rb
+++ app/screens/level.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Level_1 < Screen
+class Level < Screen
   def draw
     outputs.labels << [640, 400, "Level 1 let's goooo", 5, 1]
   end
--- app/screens/title_screen.rb
+++ app/screens/title_screen.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
-require_relative 'level_1'
+require_relative 'level'
 
 class TitleScreen < Screen
   def draw
-    outputs.labels << [640, 400, "Spacebeing in a Spiky World", 5, 1]
-    outputs.labels << [640, 40, "tick_count: #{state.tick_count}", 5, 1]
+    render_background
+    outputs.sprites << [100, 420, 1082, 93, "sprites/gosu/logo.png" ]
+    outputs.labels << [520, 400, "By Tyler and Will", 5]
+    outputs.labels << [500, 140, "Click anywhere to play", 4]
   end
 
   def handle_input
-    state.screen = Level_1.new if inputs.mouse.click
+    state.screen = Level.new if inputs.mouse.click
+  end
+
+  def render_background
+    path = 'sprites/gosu/background/colored_grass.png'
+    outputs.sprites << scrolling_background(state.tick_count, path, 1.0)
+  end
+
+  def scrolling_background(at, path, rate, y = -100)
+    image_x = 1024
+    [
+      { x: 0 - at.*(rate) % image_x, y: y, w: image_x, h: image_x, path: path },
+      { x: image_x - at.*(rate) % image_x, y: y, w: image_x, h: image_x, path: path }
+    ]
   end
 end
